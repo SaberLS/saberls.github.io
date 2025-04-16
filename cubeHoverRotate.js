ccb24f42835b8ea52d59c7caf1505a21ab7cd8c4
SaberLS <patryk.platek19@gmail.com>
2025-04-16 16:36:00 +0200

    moblie pointers

--- cubeHoverRotate.js
+++ cubeHoverRotate.js
@@ -1,5 +1,3 @@
-// import getTransitionDuration from './getTransitionDuration.js'
-
 /**
  * Represents a 3D Cube with interactive features.
  *
@@ -7,31 +5,68 @@
  * @param {HTMLElement} cube - The HTML element representing the cube.
  */
 class Cube {
-  constructor(cube) {
+  constructor (cube) {
+
     /**  @member {HTMLElement} - The HTML element representing the cube.*/
-    this.main = cube;
+    this.main = cube
 
     /**  @member {number} - `timeoutID` used to `cancelTimeout()` delayed rotation */
-    this.willRotate = 0;
+    this.willRotate = 0
 
     /**  @member {number} - `timeoutID` used to `cancelTimeout()` delayed rotation */
-    this.transitionDuration = this.getTransitionDuration();
+    this.transitionDuration = this.getTransitionDuration ()
 
     // ----------------- Events ----------------- \\
-    this.main.addEventListener('animationcancel', () => {
+    this.main.addEventListener (
+      'animationcancel',
+      () => {
       // console.log('animationcancel');
-      this.setDefaultTransform(); // 3. Starts transition which reverses the .rotate animation
-    });
+        this.setDefaultTransform () // 3. Starts transition which reverses the .rotate animation
+      },
+    )
+    if (mobileAndTabletCheck ()) {
+      console.log ('mobile')
+      this.main.addEventListener (
+        'pointerdown',
+        (event) => {
+          console.log (
+            'touchstart',
+            event,
+          )
+          this.stop ()
+          event.target.classList.add ('select')
+          this.main.classList.add ('active')
+        },
+      )
+      this.main.addEventListener (
+        'pointerup',
+        (event) => {
+          console.log (
+            'touchEnd',
+            event,
+          )
+          this.rotate ({'relatedTarget': null})
+          event.target.classList.remove ('select')
+          this.main.classList.remove ('active')
+        },
+      )
+    } else {
+      this.main.addEventListener (
+        'mouseover',
+        ({target}) => {
+          this.stop ()
+          target.classList.add ('select')
+        },
+      )
 
-    this.main.addEventListener('mouseover', ({ target }) => {
-      this.stop();
-      target.classList.add('select');
-    });
-
-    this.main.addEventListener('mouseout', (event) => {
-      this.rotate(event);
-      event.target.classList.remove('select');
-    });
+      this.main.addEventListener (
+        'mouseout',
+        (event) => {
+          this.rotate (event)
+          event.target.classList.remove ('select')
+        },
+      )
+    }
   }
 
   /**
@@ -39,20 +74,23 @@ class Cube {
    *
    * @param {HTMLElement} event.relatedTarget - The element the mouse is moving to.
    */
-  rotate = ({ relatedTarget: movedTo }) => {
-    // console.log('mouse out');
-    // console.log(movedTo);
+  rotate = ({'relatedTarget': movedTo}) => {
+    console.log ('mouse out')
+    console.log (movedTo)
 
-    this.willRotate = setTimeout(() => {
+    this.willRotate = setTimeout (
+      () => {
       // if cursor was moved out of window or on element which is not part of cube
-      if (movedTo === null || !movedTo.classList.contains('face')) {
+        if (movedTo === null || !movedTo.classList.contains ('face')) {
         // console.log('rotate');
         // console.log(this.main.style);
-        this.main.style.transform = ''; // set transform to default value
-        this.main.classList.add('rotate'); // start rotate animation
-      }
-    }, this.transitionDuration);
-  };
+          this.main.style.transform = '' // set transform to default value
+          this.main.classList.add ('rotate') // start rotate animation
+        }
+      },
+      this.transitionDuration,
+    )
+  }
 
   /**
    * Stops the cube's rotation based on its current state.
@@ -63,12 +101,14 @@ class Cube {
    * @returns {void} This method does not return a value.
    */
   stop = () => {
-    if (this.isRotating()) {
-      this.freezeRotation();
+    if (this.isRotating ()) {
+      console.log ('this Is Rotating')
+      this.freezeRotation ()
     } else {
-      this.cancelScheduledRotation();
+      console.log ('th is not Rotating')
+      this.cancelScheduledRotation ()
     }
-  };
+  }
 
   /**
    * Freezes the cube's rotation at its current animation state.
@@ -80,14 +120,14 @@ class Cube {
    *
    * @returns {String}  Transform style at which animation stopped.
    */
-  freezeRotation() {
+  freezeRotation () {
     // 1. set transform to current computed value to stop at current animation state
-    const stopAt = this.getComputedTransform();
-    this.main.style.transform = this.getComputedTransform();
+    const stopAt = this.getComputedTransform ()
+    this.main.style.transform = this.getComputedTransform ()
 
     // 2. cancel animation
-    this.main.classList.remove('rotate');
-    return stopAt;
+    this.main.classList.remove ('rotate')
+    return stopAt
   }
 
   /**
@@ -98,10 +138,10 @@ class Cube {
    *
    * @returns {void} This method does not return a value.
    */
-  cancelScheduledRotation() {
+  cancelScheduledRotation () {
     if (this.willRotate) {
-      clearTimeout(this.willRotate);
-      this.willRotate = 0;
+      clearTimeout (this.willRotate)
+      this.willRotate = 0
     }
   }
 
@@ -110,8 +150,8 @@ class Cube {
    *
    * @returns {boolean} - True if the cube is rotating, false otherwise.
    */
-  isRotating() {
-    return this.main.classList.contains('rotate');
+  isRotating () {
+    return this.main.classList.contains ('rotate')
   }
 
   /**
@@ -119,8 +159,8 @@ class Cube {
    *
    * @returns {string} - The current computed transform value.
    */
-  getComputedTransform() {
-    return window.getComputedStyle(this.main).transform;
+  getComputedTransform () {
+    return window.getComputedStyle (this.main).transform
   }
 
   /**
@@ -131,8 +171,8 @@ class Cube {
    *
    * @returns {void} This method does not return a value.
    */
-  setDefaultTransform() {
-    this.main.style.transform = '';
+  setDefaultTransform () {
+    this.main.style.transform = ''
   }
 
   /**
@@ -140,8 +180,8 @@ class Cube {
    *
    * @returns {number} - The transition duration in milliseconds.
    */
-  getTransitionDuration() {
-    return getTransitionDuration(this.main);
+  getTransitionDuration () {
+    return getTransitionDuration (this.main)
   }
 }
 
@@ -151,18 +191,35 @@ class Cube {
  * @param {HTMLElement} element - The HTML element to check for transition duration.
  * @returns {number} The transition duration in milliseconds.
  */
-function getTransitionDuration(element) {
+function getTransitionDuration (element) {
   // This returns a string representing the duration, typically in seconds (e.g., "0.5s").
   const durationString =
-    window.getComputedStyle(element)['transition-duration'];
+    window.getComputedStyle (element)['transition-duration']
 
   // If the element does not have a transition-duration, it returns 0.
-  if (durationString === '') return 0;
+  if (durationString === '') return 0
 
   // Convert the string duration to milliseconds by removing the "s" suffix and multiplying by 1000.
-  const timeout = durationString.slice(0, durationString.length - 1) * 1000;
-  return timeout;
+  const timeout = durationString.slice (
+    0,
+    durationString.length - 1,
+  ) * 1000
+  return timeout
 }
 
 // eslint-disable-next-line no-unused-vars
-const cube = new Cube(document.querySelector('.cube'));
+const cube = new Cube (document.querySelector ('.cube'))
+
+
+function mobileAndTabletCheck () {
+  const agent = navigator.userAgent || navigator.vendor || window.opera
+
+  // eslint-disable-next-line no-useless-escape
+  const isMoblie = (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i).test (agent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
+
+  return isMoblie.test (agent.substr (
+    0,
+    4,
+  ))
+}
+
